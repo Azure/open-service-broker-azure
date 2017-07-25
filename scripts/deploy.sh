4679295ab8e78dfd6ab0d8ae8d5e44eb8e4decea
Kent Rancourt <kent.rancourt@microsoft.com>
2017-07-25 14:28:09 -0400

    enable ci in travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,16 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export REGISTRY=microsoft/
-
-docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+# docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker login -u "${QUAY_USERNAME}" -p "${QUAY_PASSWORD}" quay.io
 
 if [[ "${TRAVIS_TAG}" =~ ^v[0-9]+\.[0-9]+\.[0-9]+[a-z]*$ ]]; then
     echo "Pushing images with tags '${TRAVIS_TAG}' and 'latest'."
-    VERSION="${TRAVIS_TAG}" MUTABLE_TAG="latest" make docker-push
+    REGISTRY=quay.io/deis/ VERSION="${TRAVIS_TAG}" MUTABLE_TAG="latest" \
+      make docker-push
 elif [[ "${TRAVIS_BRANCH}" == "master" ]]; then
     echo "Pushing images with default tags (git sha and 'canary')."
-    make docker-push
+    REGISTRY=quay.io/deisci/ make docker-push
 else
     echo "Skipping deployment from non-master branch"
 fi
