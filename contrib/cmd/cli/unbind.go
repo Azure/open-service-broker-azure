f1273ef68a05116dfd0fdd27adaef0d22676ecd1
Kent Rancourt <kent.rancourt@microsoft.com>
2017-07-31 12:37:50 -0400

    add unbinding

--- contrib/cmd/cli/unbind.go
+++ contrib/cmd/cli/unbind.go
@@ -1,8 +1,34 @@
 package main
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
 
-// TODO: Placeholder
+	"github.com/Azure/azure-service-broker/contrib/pkg/client"
+	"github.com/urfave/cli"
+)
+
 func unbind(c *cli.Context) error {
+	host := c.GlobalString(flagHost)
+	port := c.GlobalInt(flagPort)
+	username := c.GlobalString(flagUsername)
+	password := c.GlobalString(flagPassword)
+	instanceID := c.String(flagInstanceID)
+	if instanceID == "" {
+		return fmt.Errorf("--%s is a required flag", flagInstanceID)
+	}
+	bindingID := c.String(flagBindingID)
+	if bindingID == "" {
+		return fmt.Errorf("--%s is a required flag", flagBindingID)
+	}
+	err := client.Unbind(host, port, username, password, instanceID, bindingID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf(
+		"\nUnbound binding %s to service instance %s\n\n",
+		bindingID,
+		instanceID,
+	)
 	return nil
 }
--- contrib/pkg/client/unbind.go
+++ contrib/pkg/client/unbind.go
@@ -1,3 +1,47 @@
 package client
 
-// TODO: Placeholder
+import (
+	"fmt"
+	"net/http"
+)
+
+// Unbind carries out unbinding
+func Unbind(
+	host string,
+	port int,
+	username string,
+	password string,
+	instanceID string,
+	bindingID string,
+) error {
+	url := fmt.Sprintf(
+		"%s/v2/service_instances/%s/service_bindings/%s",
+		getBaseURL(host, port),
+		instanceID,
+		bindingID,
+	)
+	req, err := http.NewRequest(
+		http.MethodDelete,
+		url,
+		nil,
+	)
+	if err != nil {
+		return fmt.Errorf("error building request: %s", err)
+	}
+	if username != "" || password != "" {
+		addAuthHeader(req, username, password)
+	}
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error executing unbind call: %s", err)
+	}
+	defer resp.Body.Close() // nolint: errcheck
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf(
+			"unanticipated http response code %d",
+			resp.StatusCode,
+		)
+	}
+	return nil
+}
