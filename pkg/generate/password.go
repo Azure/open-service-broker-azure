c6701d33f43be65fe30fcd41ae920c5122c1da9f
Kent Rancourt <kent.rancourt@microsoft.com>
2017-08-28 18:47:48 -0500

    generate passwords that meet complexity requirements (#98)

--- pkg/generate/password.go
+++ pkg/generate/password.go
@@ -8,8 +8,22 @@ const (
 // NewPassword generates a strong, random password
 func NewPassword() string {
 	b := make([]byte, passwordLength)
-	for i := range b {
+	// Passwords need to include at least one character from each of the three
+	// groups. To ensure that, we'll fill each of the first three []byte elements
+	// with a random character from a specific group.
+	b[0] = lowerAlphaChars[seededRand.Intn(len(lowerAlphaChars))]
+	b[1] = upperAlphaChars[seededRand.Intn(len(upperAlphaChars))]
+	b[2] = numberChars[seededRand.Intn(len(numberChars))]
+	// The remainder of the characters can be completely random and drawn from
+	// all three character groups.
+	for i := 3; i < passwordLength; i++ {
 		b[i] = passwordChars[seededRand.Intn(len(passwordChars))]
+	}
+	// For good measure, shuffle the elements of the entire []byte so that
+	// the 0 character isn't predicatably lowercase, etc...
+	for i := range b {
+		j := seededRand.Intn(len(b))
+		b[i], b[j] = b[j], b[i]
 	}
 	return string(b)
 }
--- pkg/generate/password_test.go
+++ pkg/generate/password_test.go
@@ -8,10 +8,19 @@ import (
 )
 
 func TestNewPassword(t *testing.T) {
-	regex, err := regexp.Compile(`^[a-zA-Z\d]{16}$`)
+	overallRegex, err := regexp.Compile(`^[a-zA-Z\d]{16}$`)
+	assert.Nil(t, err)
+	lowerAlphaRegex, err := regexp.Compile(`[a-z]`)
+	assert.Nil(t, err)
+	upperAlphaRegex, err := regexp.Compile(`[A-Z]`)
+	assert.Nil(t, err)
+	numericRegex, err := regexp.Compile(`\d`)
 	assert.Nil(t, err)
 	for range [100]struct{}{} {
 		password := NewPassword()
-		assert.True(t, regex.MatchString(password))
+		assert.True(t, overallRegex.MatchString(password))
+		assert.True(t, lowerAlphaRegex.MatchString(password))
+		assert.True(t, upperAlphaRegex.MatchString(password))
+		assert.True(t, numericRegex.MatchString(password))
 	}
 }
