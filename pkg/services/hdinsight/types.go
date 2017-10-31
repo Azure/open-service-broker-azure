b12ddcec5ecea8f810922634264c6cf2215df47f
zhongyi-zhang <zhongz@microsoft.com>
2017-10-31 11:12:34 +0800

    rebase current master

--- pkg/services/hdinsight/types.go
+++ pkg/services/hdinsight/types.go
@@ -49,6 +49,12 @@ func (
 	return &ProvisioningParameters{}
 }
 
+// SetResourceGroup sets the name of the resource group into which service
+// instances will be deployed
+func (p *ProvisioningParameters) SetResourceGroup(resourceGroup string) {
+	p.ResourceGroup = resourceGroup
+}
+
 func (
 	s *serviceManager,
 ) GetEmptyProvisioningContext() service.ProvisioningContext {
--- tests/lifecycle/hdinsight_cases_test.go
+++ tests/lifecycle/hdinsight_cases_test.go
@@ -10,6 +10,7 @@ import (
 
 func getHDInsightCases(
 	armDeployer arm.Deployer,
+	resourceGroup string,
 ) ([]moduleLifecycleTestCase, error) {
 	hdinsightManager, err := hd.NewManager()
 	if err != nil {
@@ -18,9 +19,10 @@ func getHDInsightCases(
 
 	return []moduleLifecycleTestCase{
 		{ // hadoop
-			module:    hdinsight.New(armDeployer, hdinsightManager),
-			serviceID: "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
-			planID:    "fab80e5a-54c8-45e3-a466-f390de04e592",
+			module:      hdinsight.New(armDeployer, hdinsightManager),
+			description: "Hadoop",
+			serviceID:   "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
+			planID:      "fab80e5a-54c8-45e3-a466-f390de04e592",
 			standardProvisioningContext: service.StandardProvisioningContext{
 				Location: "southcentralus",
 			},
@@ -30,9 +32,10 @@ func getHDInsightCases(
 			bindingParameters: &hdinsight.BindingParameters{},
 		},
 		{ // spark
-			module:    hdinsight.New(armDeployer, hdinsightManager),
-			serviceID: "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
-			planID:    "9815959a-35d2-4bf7-b467-3e77c03dcc3e",
+			module:      hdinsight.New(armDeployer, hdinsightManager),
+			description: "Spark",
+			serviceID:   "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
+			planID:      "9815959a-35d2-4bf7-b467-3e77c03dcc3e",
 			standardProvisioningContext: service.StandardProvisioningContext{
 				Location: "eastus",
 			},
@@ -42,9 +45,10 @@ func getHDInsightCases(
 			bindingParameters: &hdinsight.BindingParameters{},
 		},
 		{ // hbase
-			module:    hdinsight.New(armDeployer, hdinsightManager),
-			serviceID: "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
-			planID:    "609c6d56-851e-41cf-8a71-2dde705cf5a5",
+			module:      hdinsight.New(armDeployer, hdinsightManager),
+			description: "HBase",
+			serviceID:   "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
+			planID:      "609c6d56-851e-41cf-8a71-2dde705cf5a5",
 			standardProvisioningContext: service.StandardProvisioningContext{
 				Location: "eastus2",
 			},
@@ -54,9 +58,10 @@ func getHDInsightCases(
 			bindingParameters: &hdinsight.BindingParameters{},
 		},
 		{ // storm
-			module:    hdinsight.New(armDeployer, hdinsightManager),
-			serviceID: "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
-			planID:    "ebd2dcf7-c586-42b7-8eeb-06e5641a34aa",
+			module:      hdinsight.New(armDeployer, hdinsightManager),
+			description: "Storm",
+			serviceID:   "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
+			planID:      "ebd2dcf7-c586-42b7-8eeb-06e5641a34aa",
 			standardProvisioningContext: service.StandardProvisioningContext{
 				Location: "westus",
 			},
@@ -66,13 +71,15 @@ func getHDInsightCases(
 			bindingParameters: &hdinsight.BindingParameters{},
 		},
 		{ // kafka
-			module:    hdinsight.New(armDeployer, hdinsightManager),
-			serviceID: "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
-			planID:    "c5f8277b-0cb1-4cfe-863d-03054493368a",
+			module:      hdinsight.New(armDeployer, hdinsightManager),
+			description: "Kfaka",
+			serviceID:   "c0fba6e1-4ce4-4d93-b751-c8c5e337739c",
+			planID:      "c5f8277b-0cb1-4cfe-863d-03054493368a",
+			standardProvisioningContext: service.StandardProvisioningContext{
+				Location: "northcentralus",
+			},
 			provisioningParameters: &hdinsight.ProvisioningParameters{
-				Location:               "westcentralus",
-				ResourceGroup:          newTestResourceGroupName(),
-				ClusterWorkerNodeCount: 2,
+				ClusterWorkerNodeCount: 1,
 			},
 			bindingParameters: &hdinsight.BindingParameters{},
 		},
