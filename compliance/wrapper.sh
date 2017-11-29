d5bb4fb71d4a3507d892b1a4d312d1cef18737c5
Jeremy Rickard <jeremy.rickard@microsoft.com>
2017-11-29 14:20:09 -0700

    Initial changes to enable use of osb-checker as part of CI process.
    This adds a few things in order to enable us to begin using the
    osb-checker to verify complaince with the OSB spec.
    
    1.) Define a new dockerfile that builds an NPM based container with
        a.) the osb-checker code cloned into the repository
            we should probably take some approach at vendoring this later
    
        b.) a config file with our fake plan/service ids along with test
            connection info for the broker. The osb-checker currently
            expects the file to be called configs_mock.json or requires
            a code change, so the file is added to the container for that.
    
        c.) A wrapper is added to ensure the test broker is up
    
    2.) A new test with build tag compliance that starts the broker up
        with basic auth enabled but the mock service and mock asynch engine
    
    3.) Changes to docker compose to start the test broker with appropriate
        go test flags and then to start the osb-checker container
    
    4.) New target in the Makefile 'test-api-compliance'. This currently
        ignores the exit code of the docker-compose recipe because there are
        some issues with the osb-checker currently (and some issues we need to fix).

---
+++ compliance/wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#wrapper.sh
+
+set -e
+
+until [ 200 -eq $(curl --write-out %{http_code} --silent --output /dev/null http://broker:8080/healthz) ]; do
+    echo "Waiting for server to be up"
+    sleep 5
+done
+
+bash -c mocha
