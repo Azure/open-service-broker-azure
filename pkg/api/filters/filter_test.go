d32f4a947d67cabc5a504c24556a1fe345d7c3ed
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-01-22 10:46:12 -0700

    Added test for filter chain

---
+++ pkg/api/filters/filter_test.go
@@ -0,0 +1,107 @@
+package filters
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type failingFilter struct{
+	called bool
+}
+
+func (f *failingFilter) Execute(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		f.called = true
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
+type passingFilter struct{
+	called bool
+}
+
+func (p *passingFilter) Execute(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		p.called = true
+		handler(w,r)
+	}
+}
+
+func TestFilterChainWithNoFilters(t *testing.T) {
+	var handlerCalled = false
+	f1 := func(w http.ResponseWriter, r *http.Request) {
+		handlerCalled = true
+	}
+	filterChain :=  NewFilterChain([]Filter{})
+	filtered := filterChain.Filter(f1)
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+	rr := httptest.NewRecorder()
+	filtered(rr,req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.True(t, handlerCalled)
+}
+
+func TestFilterChainWithFailingFilter(t *testing.T) {
+	failing := &failingFilter{}
+	var handlerCalled = false
+	f1 := func(w http.ResponseWriter, r *http.Request) {
+		handlerCalled = true
+	}
+	filterChain :=  NewFilterChain([]Filter{
+		failing,
+	})
+	filtered := filterChain.Filter(f1)
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+	rr := httptest.NewRecorder()
+	filtered(rr,req)
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	assert.False(t, handlerCalled)
+	assert.True(t, failing.called)
+}
+
+func TestFilterChainWithPassingFilter(t *testing.T) {
+	passing := &passingFilter{}
+	var handlerCalled = false
+	f1 := func(w http.ResponseWriter, r *http.Request) {
+		handlerCalled = true
+	}
+	filterChain :=  NewFilterChain([]Filter{
+		passing,
+	})
+	filtered := filterChain.Filter(f1)
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+	rr := httptest.NewRecorder()
+	filtered(rr,req)
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.True(t, handlerCalled)
+	assert.True(t, passing.called)
+}
+
+func TestFilterChainWithMixedFilters(t *testing.T) {
+	passing := &passingFilter{}
+	failing := &failingFilter{}
+	var handlerCalled = false
+	f1 := func(w http.ResponseWriter, r *http.Request) {
+		handlerCalled = true
+	}
+	filterChain :=  NewFilterChain([]Filter{
+		passing,
+		failing,
+	})
+	filtered := filterChain.Filter(f1)
+	req, err := http.NewRequest(http.MethodGet, "/", nil)
+	assert.Nil(t, err)
+	rr := httptest.NewRecorder()
+	filtered(rr,req)
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	assert.False(t, handlerCalled)
+	assert.True(t, passing.called)
+	assert.True(t, failing.called)
+}
