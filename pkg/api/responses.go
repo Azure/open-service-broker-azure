91e5ee86cc0d9469921ec6539be70401f43b04af
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-01-25 16:49:06 -0700

    Replacing empty json responses with error messages.
    Issue #66 points out that our error messages don't help determine the
    cause of operation failures. This PR adds an informative response to
    locations where we are returning a 400 or a 409 so the user has some extra infromation
    to debug the failure.
    
    These should be inline with the Broker Errors section of the OSB spec.

--- pkg/api/responses.go
+++ pkg/api/responses.go
@@ -107,6 +107,7 @@ func generateOperationFailedResponse() []byte {
 
 var responseEmptyJSON = []byte("{}")
 
+<<<<<<< HEAD
 func generateEmptyResponse() []byte {
 	return responseEmptyJSON
 }
@@ -118,6 +119,11 @@ func generateConflictResponse() []byte {
 	return responseConflict
 }
 
+=======
+var responseConflict = []byte(`{ "description": "A service instance exists ` +
+	`with the specified service id" }`)
+
+>>>>>>> Replacing empty json responses with error messages.
 // The following are custom to this broker-- i.e. not explicitly declared by
 // the OSB spec
 
