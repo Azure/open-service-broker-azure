01d84755f1ca091d2ad841ed9656bb42d64eba23
Kent Rancourt <kent.rancourt@microsoft.com>
2018-02-08 14:48:57 -0500

    support CF-specific env vars for configuring basic auth via tiles

--- pkg/config/basic_auth.go
+++ pkg/config/basic_auth.go
@@ -1,6 +1,8 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import (
+	"github.com/kelseyhightower/envconfig"
+)
 
 // BasicAuthConfig represents details such as username and password that will
 // be used to secure the broker using basic auth
@@ -10,14 +12,28 @@ type BasicAuthConfig interface {
 }
 
 type basicAuthConfig struct {
-	Username string `envconfig:"BASIC_AUTH_USERNAME" required:"true"`
-	Password string `envconfig:"BASIC_AUTH_PASSWORD" required:"true"`
+	Username   string `envconfig:"BASIC_AUTH_USERNAME"`
+	CFUsername string `envconfig:"SECURITY_USER_NAME"`
+	Password   string `envconfig:"BASIC_AUTH_PASSWORD"`
+	CFPassword string `envconfig:"SECURITY_USER_PASSWORD"`
 }
 
 // GetBasicAuthConfig returns basic auth configuration
 func GetBasicAuthConfig() (BasicAuthConfig, error) {
 	bac := basicAuthConfig{}
 	err := envconfig.Process("", &bac)
+	if bac.Username == "" && bac.CFUsername == "" {
+		return bac, &errBasicAuthUsernameNotSpecified{}
+	}
+	if bac.Username == "" && bac.CFUsername != "" {
+		bac.Username = bac.CFUsername
+	}
+	if bac.Password == "" && bac.CFPassword == "" {
+		return bac, &errBasicAuthPasswordNotSpecified{}
+	}
+	if bac.Password == "" && bac.CFPassword != "" {
+		bac.Password = bac.CFPassword
+	}
 	return bac, err
 }
 
---
+++ pkg/config/basic_auth_test.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicAuthUsernameNotSpecified(t *testing.T) {
+	err := os.Setenv("BASIC_AUTH_USERNAME", "")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_NAME", "")
+	assert.Nil(t, err)
+	_, err = GetBasicAuthConfig()
+	assert.IsType(t, &errBasicAuthUsernameNotSpecified{}, err)
+}
+
+func TestBasicAuthPasswordNotSpecified(t *testing.T) {
+	err := os.Setenv("BASIC_AUTH_USERNAME", "foo")
+	assert.Nil(t, err)
+	err = os.Setenv("BASIC_AUTH_PASSWORD", "")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_PASSWORD", "")
+	assert.Nil(t, err)
+	_, err = GetBasicAuthConfig()
+	assert.IsType(t, &errBasicAuthPasswordNotSpecified{}, err)
+}
+
+func TestBasicAuthUsernamePasswordPrecedence(t *testing.T) {
+	err := os.Setenv("BASIC_AUTH_USERNAME", "foo")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_NAME", "foo2")
+	assert.Nil(t, err)
+	err = os.Setenv("BASIC_AUTH_PASSWORD", "bar")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_PASSWORD", "bar2")
+	assert.Nil(t, err)
+	bac, err := GetBasicAuthConfig()
+	assert.Nil(t, err)
+	assert.Equal(t, "foo", bac.GetUsername())
+	assert.Equal(t, "bar", bac.GetPassword())
+}
+
+func TestBasicAuthUsernamePasswordFallback(t *testing.T) {
+	err := os.Setenv("BASIC_AUTH_USERNAME", "")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_NAME", "foo2")
+	assert.Nil(t, err)
+	err = os.Setenv("BASIC_AUTH_PASSWORD", "")
+	assert.Nil(t, err)
+	err = os.Setenv("SECURITY_USER_PASSWORD", "bar2")
+	assert.Nil(t, err)
+	bac, err := GetBasicAuthConfig()
+	assert.Nil(t, err)
+	assert.Equal(t, "foo2", bac.GetUsername())
+	assert.Equal(t, "bar2", bac.GetPassword())
+}
