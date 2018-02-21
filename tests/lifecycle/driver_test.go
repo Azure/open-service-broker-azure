85781de9ba9c048da3c47b3906cd8b60e41e6cdd
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-02-21 16:08:13 -0700

    Add some timeout and periodic status messages for the lifecycle tests

--- tests/lifecycle/driver_test.go
+++ tests/lifecycle/driver_test.go
@@ -5,6 +5,7 @@ package lifecycle
 import (
 	"log"
 	"testing"
+	"time"
 
 	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
@@ -23,14 +24,42 @@ func TestServices(t *testing.T) {
 	// Make sure we clean up after ourselves
 	defer func() {
 		log.Printf("----> deleting resource group \"%s\"\n", resourceGroup)
-		if err = deleteResourceGroup(resourceGroup); err != nil {
-			log.Printf("----> error deleting resource group: %s", err)
-		} else {
-			log.Printf(
-				"----> deleted resource group \"%s\"\n",
-				resourceGroup,
-			)
+		done := make(chan struct{})
+		failed := make(chan error)
+		t := time.NewTicker(time.Minute * 5).C
+		timeout := time.NewTimer(time.Minute * 30).C
+		go func() {
+			if err = deleteResourceGroup(resourceGroup); err != nil {
+				failed <- err
+			} else {
+				done <- struct{}{}
+			}
+		}()
+		for {
+			select {
+			case <-failed:
+				log.Printf("----> error deleting resource group: %s", err)
+				return
+			case <-done:
+				log.Printf(
+					"----> deleted resource group \"%s\"\n",
+					resourceGroup,
+				)
+				return
+			case <-t:
+				//Periodically emit a message
+				log.Printf(
+					"----> delete resource group \"%s\": in progress\n",
+					resourceGroup,
+				)
+			case <-timeout:
+				//Also use a timeout channel to enforce some (un)reasonable
+				//lenght for the delete RG to get killed in
+				log.Printf("----> error deleting resource group: timeout")
+				return
+			}
 		}
+
 	}()
 
 	testCases, err := getTestCases()
