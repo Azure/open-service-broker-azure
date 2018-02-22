3c331b3850e92fec1243e236960c4d094d4f88f7
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-02-21 21:49:31 -0700

    Refactored tests to  use testing.M. Refactored RG creation to setup and deletion to teardown.

--- tests/lifecycle/driver_test.go
+++ tests/lifecycle/driver_test.go
@@ -4,6 +4,7 @@ package lifecycle
 
 import (
 	"log"
+	"os"
 	"testing"
 	"time"
 
@@ -11,56 +12,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var resourceGroup string
+
 func TestServices(t *testing.T) {
-	resourceGroup := "test-" + uuid.NewV4().String()
-
-	log.Printf("----> creating resource group \"%s\"\n", resourceGroup)
-	err := ensureResourceGroup(resourceGroup)
-	assert.Nil(t, err)
-	log.Printf("----> created resource group \"%s\"\n", resourceGroup)
-
 	log.Printf("----> testing in resource group \"%s\"\n", resourceGroup)
-
-	// Make sure we clean up after ourselves
-	defer func() {
-		log.Printf("----> deleting resource group \"%s\"\n", resourceGroup)
-		done := make(chan struct{})
-		failed := make(chan error)
-		t := time.NewTicker(time.Minute * 5).C
-		timeout := time.NewTimer(time.Minute * 30).C
-		go func() {
-			if err = deleteResourceGroup(resourceGroup); err != nil {
-				failed <- err
-			} else {
-				done <- struct{}{}
-			}
-		}()
-		for {
-			select {
-			case <-failed:
-				log.Printf("----> error deleting resource group: %s", err)
-				return
-			case <-done:
-				log.Printf(
-					"----> deleted resource group \"%s\"\n",
-					resourceGroup,
-				)
-				return
-			case <-t:
-				//Periodically emit a message
-				log.Printf(
-					"----> delete resource group \"%s\": in progress\n",
-					resourceGroup,
-				)
-			case <-timeout:
-				//Also use a timeout channel to enforce some (un)reasonable
-				//lenght for the delete RG to get killed in
-				log.Printf("----> error deleting resource group: timeout")
-				return
-			}
-		}
-
-	}()
 
 	testCases, err := getTestCases()
 	assert.Nil(t, err)
@@ -81,4 +36,66 @@ func TestServices(t *testing.T) {
 		}
 	})
 
+}
+
+func TestMain(m *testing.M) {
+	if err := setup(); err != nil {
+		os.Exit(-1)
+	}
+	exitCode := m.Run()
+	if err := tearDown(); err != nil {
+		os.Exit(-1)
+	}
+	os.Exit(exitCode)
+}
+
+func setup() error {
+	resourceGroup = "test-" + uuid.NewV4().String()
+
+	log.Printf("----> creating resource group \"%s\"\n", resourceGroup)
+	if err := ensureResourceGroup(resourceGroup); err != nil {
+		return err
+	}
+	log.Printf("----> created resource group \"%s\"\n", resourceGroup)
+
+	return nil
+}
+
+func tearDown() error {
+	log.Printf("----> deleting resource group \"%s\"\n", resourceGroup)
+	done := make(chan struct{})
+	failed := make(chan error)
+	t := time.NewTicker(time.Minute * 5).C
+	timeout := time.NewTimer(time.Minute * 30).C
+	go func() {
+		if err := deleteResourceGroup(resourceGroup); err != nil {
+			failed <- err
+		} else {
+			done <- struct{}{}
+		}
+	}()
+	for {
+		select {
+		case err := <-failed:
+			log.Printf("----> error deleting resource group: %s", err)
+			return err
+		case <-done:
+			log.Printf(
+				"----> deleted resource group \"%s\"\n",
+				resourceGroup,
+			)
+			return nil
+		case <-t:
+			//Periodically emit a message
+			log.Printf(
+				"----> delete resource group \"%s\": in progress\n",
+				resourceGroup,
+			)
+		case <-timeout:
+			//Also use a timeout channel to enforce some (un)reasonable
+			//lenght for the delete RG to get killed in
+			log.Printf("----> error deleting resource group: timeout")
+			return nil
+		}
+	}
 }
