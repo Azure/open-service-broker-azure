645df9559ed60414dc96479f2b58d130af28099a
Kent Rancourt <kent.rancourt@microsoft.com>
2018-02-27 15:09:14 -0500

    cli: short-circuit param parsing if there are none

--- contrib/cmd/cli/params.go
+++ contrib/cmd/cli/params.go
@@ -10,8 +10,12 @@ import (
 // parseParams iterates, in turn, over string, int, float, and bool params as
 // specified by the user and parses them into a map[string]interface{}.
 func parseParams(c *cli.Context) (map[string]interface{}, error) {
-	jsonBytes := []byte(c.String(flagParameters))
 	params := map[string]interface{}{}
+	paramsStr := c.String(flagParameters)
+	if paramsStr == "" {
+		return params, nil
+	}
+	jsonBytes := []byte(paramsStr)
 	err := json.Unmarshal(jsonBytes, &params)
 	if err != nil {
 		return nil, fmt.Errorf("error parsing JSON parameters: %s", err)
