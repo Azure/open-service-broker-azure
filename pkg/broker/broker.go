bafcf893f2fbd5b1e5e4ae50aca11da5e3ff4fbf
Kent Rancourt <kent.rancourt@microsoft.com>
2018-03-12 13:48:17 -0400

    refactor how (where) the catalog is built (#337)

--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -52,43 +52,10 @@ func NewBroker(
 	asyncEngine async.Engine,
 	codec crypto.Codec,
 	filterChain filter.Filter,
-	modules []service.Module,
-	minStability service.Stability,
+	catalog service.Catalog,
 	defaultAzureLocation string,
 	defaultAzureResourceGroup string,
 ) (Broker, error) {
-	// Consolidate the catalogs from all the individual modules into a single
-	// catalog. Check as we go along to make sure that no two modules provide
-	// services having the same ID.
-	services := []service.Service{}
-	usedServiceIDs := map[string]string{}
-	for _, module := range modules {
-		if module.GetStability() >= minStability {
-			moduleName := module.GetName()
-			catalog, err := module.GetCatalog()
-			if err != nil {
-				return nil, fmt.Errorf(
-					`error retrieving catalog from module "%s": %s`,
-					moduleName,
-					err,
-				)
-			}
-			for _, svc := range catalog.GetServices() {
-				serviceID := svc.GetID()
-				if moduleNameForUsedServiceID, ok := usedServiceIDs[serviceID]; ok {
-					return nil, fmt.Errorf(
-						`modules "%s" and "%s" both provide a service with the id "%s"`,
-						moduleNameForUsedServiceID,
-						moduleName,
-						serviceID,
-					)
-				}
-				services = append(services, svc)
-				usedServiceIDs[serviceID] = moduleName
-			}
-		}
-	}
-	catalog := service.NewCatalog(services)
 	b := &broker{
 		store:       storage.NewStore(storageRedisClient, catalog, codec),
 		asyncEngine: asyncEngine,
