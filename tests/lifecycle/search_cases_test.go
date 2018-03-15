a20595d5e857e8cd8cece3f865e4f85ff9adffe4
Kent Rancourt <kent.rancourt@microsoft.com>
2018-03-14 22:40:45 -0400

    refactor search

--- tests/lifecycle/search_cases_test.go
+++ tests/lifecycle/search_cases_test.go
@@ -23,12 +23,10 @@ func getSearchCases(
 	servicesClient.Authorizer = authorizer
 	return []serviceLifecycleTestCase{
 		{
-			module:                 search.New(armDeployer, servicesClient),
-			serviceID:              "c54902aa-3027-4c5c-8e96-5b3d3b452f7f",
-			planID:                 "35bd6e80-5ff5-487e-be0e-338aee9321e4",
-			location:               "southcentralus",
-			provisioningParameters: &search.ProvisioningParameters{},
-			bindingParameters:      &search.BindingParameters{},
+			module:    search.New(armDeployer, servicesClient),
+			serviceID: "c54902aa-3027-4c5c-8e96-5b3d3b452f7f",
+			planID:    "35bd6e80-5ff5-487e-be0e-338aee9321e4",
+			location:  "southcentralus",
 		},
 	}, nil
 }
