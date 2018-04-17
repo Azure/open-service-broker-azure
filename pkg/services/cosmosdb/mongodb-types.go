ee3ed6da8039e962bb309899e10e834f68e78053
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-04-17 14:40:33 -0600

    Code review comments.

--- pkg/services/cosmosdb/mongodb-types.go
+++ pkg/services/cosmosdb/mongodb-types.go
@@ -3,10 +3,10 @@ package cosmosdb
 // mongoCredentials encapsulates CosmosDB-specific connection details and
 // credentials for connecting with the MongoDB API.
 type mongoCredentials struct {
-	Host             string `json:"host,omitempty"`
-	Port             int    `json:"port,omitempty"`
-	Username         string `json:"username,omitempty"`
-	Password         string `json:"password,omitempty"`
-	ConnectionString string `json:"connectionString,omitempty"`
-	URI              string `json:"uri,omitempty"`
+	Host             string `json:"host"`
+	Port             int    `json:"port"`
+	Username         string `json:"username"`
+	Password         string `json:"password"`
+	ConnectionString string `json:"connectionString"`
+	URI              string `json:"uri"`
 }
