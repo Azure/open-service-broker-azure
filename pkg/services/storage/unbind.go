3ea4c20a3bc48e691ffeba56adefb49b24e15fef
Kent Rancourt <kent.rancourt@microsoft.com>
2018-05-17 11:55:38 -0400

    UNDO ME LATER-- remove most modules to limit distraction as I refactor

--- pkg/services/storage/unbind.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import (
-	"github.com/Azure/open-service-broker-azure/pkg/service"
-)
-
-func (s *serviceManager) Unbind(service.Instance, service.Binding) error {
-	return nil
-}
