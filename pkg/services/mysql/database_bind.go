0adcee62b77e92873e9be90a9abbf6344b556953
Kent Rancourt <kent.rancourt@microsoft.com>
2018-05-18 21:09:27 -0400

    mysql update capability (#455)
    * s/planSchema/tierDetails/
    
    * default firewall rules do not vary by tier
    
    * ssl options do not vary by tier
    
    * default hardware does not vary by tier
    
    * min storage does not vary by tier
    
    * default storage does not vary by tier
    
    * default backup redundancy does not vary by tier
    
    * min backup retention does not vary by tier
    
    * max backup retention does not vary by tier
    
    * default backup retention does not vary by tier
    
    * clean up tier names
    
    * add schema and validation for updates
    
    * add update capability

--- pkg/services/mysql/database_bind.go
+++ pkg/services/mysql/database_bind.go
@@ -30,9 +30,8 @@ func (d *databaseManager) Bind(
 	); err != nil {
 		return nil, nil, err
 	}
-	pSchema := instance.Parent.Plan.GetProperties().Extended["provisionSchema"].(planSchema) // nolint: lll
 	return createBinding(
-		pSchema.isSSLRequired(ppp),
+		isSSLRequired(ppp),
 		d.sqlDatabaseDNSSuffix,
 		pdt.ServerName,
 		spdt.AdministratorLoginPassword,
@@ -69,10 +68,9 @@ func (d *databaseManager) GetCredentials(
 	); err != nil {
 		return nil, err
 	}
-	pSchema := instance.Parent.Plan.GetProperties().Extended["provisionSchema"].(planSchema) // nolint: lll
 	creds := createCredential(
 		pdt.FullyQualifiedDomainName,
-		pSchema.isSSLRequired(ppp),
+		isSSLRequired(ppp),
 		pdt.ServerName,
 		dt.DatabaseName,
 		bd,
