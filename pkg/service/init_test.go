52cfde00e975a73c8bae0c35db7d3dad3b6742e6
Kent Rancourt <kent.rancourt@microsoft.com>
2018-06-14 11:58:44 -0400

    add SecureString type

--- pkg/service/init_test.go
+++ pkg/service/init_test.go
@@ -6,11 +6,19 @@ import (
 	"testing"
 
 	"github.com/Azure/open-service-broker-azure/pkg/crypto"
-	"github.com/Azure/open-service-broker-azure/pkg/crypto/noop"
+	"github.com/Azure/open-service-broker-azure/pkg/crypto/aes256"
 )
 
 func TestMain(m *testing.M) {
-	if err := crypto.InitializeGlobalCodec(noop.NewCodec()); err != nil {
+	codec, err := aes256.NewCodec(
+		aes256.Config{
+			Key: "AES256Key-32Characters1234567890",
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := crypto.InitializeGlobalCodec(codec); err != nil {
 		log.Fatal(err)
 	}
 	os.Exit(m.Run())
