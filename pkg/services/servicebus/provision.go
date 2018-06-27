830858c46d5a8847d79776ba0aee792e809bdec8
Kent Rancourt <kent.rancourt@microsoft.com>
2018-06-26 20:32:14 -0400

    re-enable servicebus module (#500)

--- pkg/services/servicebus/provision.go
+++ pkg/services/servicebus/provision.go
@@ -1,5 +1,3 @@
-// +build experimental
-
 package servicebus
 
 import (
@@ -22,59 +20,59 @@ func (s *serviceManager) GetProvisioner(
 func (s *serviceManager) preProvision(
 	context.Context,
 	service.Instance,
-) (service.InstanceDetails, service.SecureInstanceDetails, error) {
-	dt := instanceDetails{
+) (service.InstanceDetails, error) {
+	return &instanceDetails{
 		ARMDeploymentName:       uuid.NewV4().String(),
 		ServiceBusNamespaceName: "sb-" + uuid.NewV4().String(),
-	}
-	dtMap, err := service.GetMapFromStruct(dt)
-	return dtMap, nil, err
+	}, nil
 }
 
 func (s *serviceManager) deployARMTemplate(
 	_ context.Context,
 	instance service.Instance,
-) (service.InstanceDetails, service.SecureInstanceDetails, error) {
-	dt := instanceDetails{}
-	if err := service.GetStructFromMap(instance.Details, &dt); err != nil {
-		return nil, nil, err
+) (service.InstanceDetails, error) {
+	dt := instance.Details.(*instanceDetails)
+	tagsObj := instance.ProvisioningParameters.GetObject("tags")
+	tags := make(map[string]string, len(tagsObj.Data))
+	for k := range tagsObj.Data {
+		tags[k] = tagsObj.GetString(k)
 	}
 	outputs, err := s.armDeployer.Deploy(
 		dt.ARMDeploymentName,
-		instance.ResourceGroup,
-		instance.Location,
+		instance.ProvisioningParameters.GetString("resourceGroup"),
+		instance.ProvisioningParameters.GetString("location"),
 		armTemplateBytes,
-		nil, // Go template params
-		map[string]interface{}{ // ARM template params
+		map[string]interface{}{
+			"location":                instance.ProvisioningParameters.GetString("location"), // nolint: lll
 			"serviceBusNamespaceName": dt.ServiceBusNamespaceName,
 			"serviceBusSku": instance.Plan.
 				GetProperties().Extended["serviceBusSku"],
 		},
-		instance.Tags,
+		map[string]interface{}{},
+		tags,
 	)
 	if err != nil {
-		return nil, nil, fmt.Errorf("error deploying ARM template: %s", err)
+		return nil, fmt.Errorf("error deploying ARM template: %s", err)
 	}
 
-	sdt := secureInstanceDetails{}
-
 	var ok bool
-	sdt.ConnectionString, ok = outputs["connectionString"].(string)
+	connectionString, ok := outputs["connectionString"].(string)
 	if !ok {
-		return nil, nil, fmt.Errorf(
+		return nil, fmt.Errorf(
 			"error retrieving connection string from deployment: %s",
 			err,
 		)
 	}
+	dt.ConnectionString = service.SecureString(connectionString)
 
-	sdt.PrimaryKey, ok = outputs["primaryKey"].(string)
+	primaryKey, ok := outputs["primaryKey"].(string)
 	if !ok {
-		return nil, nil, fmt.Errorf(
+		return nil, fmt.Errorf(
 			"error retrieving primary key from deployment: %s",
 			err,
 		)
 	}
+	dt.PrimaryKey = service.SecureString(primaryKey)
 
-	sdtMap, err := service.GetMapFromStruct(sdt)
-	return instance.Details, sdtMap, err
+	return dt, err
 }
