71a9ac7e3477e852edd865bf6656cca153d7df9d
Jeremy Rickard <jeremy.rickard@microsoft.com>
2018-07-06 08:22:00 -0600

    Adding update schema

--- pkg/services/cosmosdb/common-update.go
+++ pkg/services/cosmosdb/common-update.go
@@ -1,6 +1,7 @@
 package cosmosdb
 
 import (
+	"context"
 	"github.com/Azure/open-service-broker-azure/pkg/service"
 )
 
@@ -10,8 +11,16 @@ func (
 	return nil
 }
 
-func (
-	c *cosmosAccountManager,
-) GetUpdater(service.Plan) (service.Updater, error) {
-	return service.NewUpdater()
+func (c *cosmosAccountManager) GetUpdater(service.Plan) (service.Updater, error) {
+	// There isn't a need to do any "pre-provision here. just the update step"
+	return service.NewUpdater(
+		service.NewUpdatingStep("updateARMTemplate", c.updateARMTemplate),
+	)
+}
+
+func (c *cosmosAccountManager) updateARMTemplate(
+	_ context.Context,
+	instance service.Instance,
+) (service.InstanceDetails, error) {
+	return instance.Details, nil
 }
