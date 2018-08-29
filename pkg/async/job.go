867ac7f0f15a9bc2afbdd7dfae88f356b6d70cba
Kent Rancourt <kent.rancourt@microsoft.com>
2018-08-28 19:57:49 -0600

    remove local async pkg

--- pkg/async/job.go
+++
@@ -1,7 +0,0 @@
-package async
-
-import "context"
-
-// JobFn is the signature for functions that workers can call to asynchronously
-// execute a job
-type JobFn func(ctx context.Context, task Task) ([]Task, error)
