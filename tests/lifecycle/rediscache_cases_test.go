e38a4737bd3dea7f152f8620448ec60f1085fb65
norshtein <tosi@microsoft.com>
2018-09-14 14:10:03 +0800

    Add lifecycle test case

--- tests/lifecycle/rediscache_cases_test.go
+++ tests/lifecycle/rediscache_cases_test.go
@@ -5,11 +5,17 @@ package lifecycle
 var rediscacheTestCases = []serviceLifecycleTestCase{
 	{
 		group:     "rediscache",
-		name:      "rediscache",
+		name:      "rediscache-basic-provision-and-update",
 		serviceID: "0346088a-d4b2-4478-aa32-f18e295ec1d9",
 		planID:    "362b3d1b-5b57-4289-80ad-4a15a760c29c",
 		provisioningParameters: map[string]interface{}{
-			"location": "southcentralus",
+			"location":         "southcentralus",
+			"skuCapacity":      1,
+			"enableNonSslPort": "disabled",
+		},
+		updatingParameters: map[string]interface{}{
+			"skuCapacity":      2,
+			"enableNonSslPort": "enabled",
 		},
 	},
 }
