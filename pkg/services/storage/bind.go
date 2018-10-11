b92afceae88ecb232607b31e9be9a5ee8be79c59
Tongyao Si <1452707@tongji.edu.cn>
2018-10-11 10:14:40 +0800

    Remove outdated property in arm template and add fields in credential (#608)
    * Remove outdated property in arm template and add fields in credential
    
    * Change the way judging storage account kind

--- pkg/services/storage/bind.go
+++ pkg/services/storage/bind.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"fmt"
+
 	"github.com/Azure/open-service-broker-azure/pkg/service"
 )
 
@@ -11,14 +13,23 @@ func (s *serviceManager) Bind(
 	return nil, nil
 }
 
+// nolint: lll
 func (s *serviceManager) GetCredentials(
 	instance service.Instance,
 	_ service.Binding,
 ) (service.Credentials, error) {
 	dt := instance.Details.(*instanceDetails)
-	return credentials{
-		StorageAccountName: dt.StorageAccountName,
-		AccessKey:          dt.AccessKey,
-		ContainerName:      dt.ContainerName,
-	}, nil
+	credential := credentials{
+		StorageAccountName:          dt.StorageAccountName,
+		AccessKey:                   dt.AccessKey,
+		ContainerName:               dt.ContainerName,
+		PrimaryBlobServiceEndPoint:  fmt.Sprintf("https://%s.blob.core.windows.net/", dt.StorageAccountName),
+		PrimaryTableServiceEndPoint: fmt.Sprintf("https://%s.table.core.windows.net/", dt.StorageAccountName),
+	}
+	storeKind, _ := instance.Plan.GetProperties().Extended[kindKey].(storageKind)
+	if storeKind == storageKindGeneralPurposeStorageAcccount {
+		credential.PrimaryFileServiceEndPoint = fmt.Sprintf("https://%s.file.core.windows.net/", dt.StorageAccountName)
+		credential.PrimaryQueueServiceEndPoint = fmt.Sprintf("https://%s.queue.core.windows.net/", dt.StorageAccountName)
+	}
+	return credential, nil
 }
