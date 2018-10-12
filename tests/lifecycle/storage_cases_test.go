7702634377ec44ebb30531415889a5639683c904
norshtein <tosi@microsoft.com>
2018-10-12 14:06:06 +0800

    Add lifecycle test for https

--- tests/lifecycle/storage_cases_test.go
+++ tests/lifecycle/storage_cases_test.go
@@ -9,7 +9,11 @@ var storageTestCases = []serviceLifecycleTestCase{
 		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
 		planID:    "e19fb0be-dd1f-4ef0-b44f-88832dca1a66",
 		provisioningParameters: map[string]interface{}{
-			"location": "westus",
+			"location":              "westus",
+			"enableNonHttpsTraffic": "enabled",
+		},
+		updatingParameters: map[string]interface{}{
+			"enableNonHttpsTraffic": "disabled",
 		},
 	},
 	{ // General Purpose Storage Account
@@ -18,7 +22,11 @@ var storageTestCases = []serviceLifecycleTestCase{
 		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
 		planID:    "6ddf6b41-fb60-4b70-af99-8ecc4896b3cf",
 		provisioningParameters: map[string]interface{}{
-			"location": "southcentralus",
+			"location":              "southcentralus",
+			"enableNonHttpsTraffic": "disabled",
+		},
+		updatingParameters: map[string]interface{}{
+			"enableNonHttpsTraffic": "enabled",
 		},
 	},
 	{ // Blob Storage Account
@@ -27,7 +35,8 @@ var storageTestCases = []serviceLifecycleTestCase{
 		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
 		planID:    "800a17e1-f20a-463d-a290-20516052f647",
 		provisioningParameters: map[string]interface{}{
-			"location": "eastus",
+			"location":              "eastus",
+			"enableNonHttpsTraffic": "enabled",
 		},
 	},
 	{ // Blob Storage Account + Blob Container
@@ -36,7 +45,8 @@ var storageTestCases = []serviceLifecycleTestCase{
 		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
 		planID:    "189d3b8f-8307-4b3f-8c74-03d069237f70",
 		provisioningParameters: map[string]interface{}{
-			"location": "southcentralus",
+			"location":              "southcentralus",
+			"enableNonHttpsTraffic": "enabled",
 		},
 	},
 }
