07557506fd925ee2021509b32c3681d75ef95993
norshtein <tosi@microsoft.com>
2018-10-15 10:38:06 +0800

    Temporarily remove failed lifecycle test case

--- tests/lifecycle/cosmosdb_cases_test.go
+++ tests/lifecycle/cosmosdb_cases_test.go
@@ -13,36 +13,6 @@ import (
 )
 
 var cosmosdbTestCases = []serviceLifecycleTestCase{
-	{ // SQL API
-		group:     "cosmosdb",
-		name:      "sql-api-account-only",
-		serviceID: "6330de6f-a561-43ea-a15e-b99f44d183e6",
-		planID:    "71168d1a-c704-49ff-8c79-214dd3d6f8eb",
-		provisioningParameters: map[string]interface{}{
-			"alias":    "cosmos-account",
-			"location": "eastus",
-			"ipFilters": map[string]interface{}{
-				"allowedIPRanges": []interface{}{"0.0.0.0/0"},
-			},
-			"consistencyPolicy": map[string]interface{}{
-				"defaultConsistencyLevel": "Session",
-			},
-		},
-		updatingParameters: map[string]interface{}{
-			"readRegions": []interface{}{"centralus"},
-		},
-		childTestCases: []*serviceLifecycleTestCase{
-			{ // database only scenario
-				group:     "cosmosdb",
-				name:      "database-only",
-				serviceID: "87c5132a-6d76-40c6-9621-0c7b7542571b",
-				planID:    "c821c68c-c8e0-4176-8cf2-f0ca582a07a3",
-				provisioningParameters: map[string]interface{}{
-					"parentAlias": "cosmos-account",
-				},
-			},
-		},
-	},
 	{ // Graph API
 		group:     "cosmosdb",
 		name:      "graph-api-account-only",
