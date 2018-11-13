15b208e4620230f5583d163bab092755b8cdb1f9
norshtein <tosi@microsoft.com>
2018-11-13 15:46:48 +0800

    Update lifecycle test

--- tests/lifecycle/storage_cases_test.go
+++ tests/lifecycle/storage_cases_test.go
@@ -6,8 +6,8 @@ var storageTestCases = []serviceLifecycleTestCase{
 	{ // General Purpose V2 Storage Account
 		group:     "storage",
 		name:      "general-purpose-v2-account",
-		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
-		planID:    "e19fb0be-dd1f-4ef0-b44f-88832dca1a66",
+		serviceID: "9a3e28fe-8c02-49da-9b35-1b054eb06c95",
+		planID:    "bc4f766a-c372-479c-b0b4-bd9d0546b3ef",
 		provisioningParameters: map[string]interface{}{
 			"location":              "eastus",
 			"enableNonHttpsTraffic": "enabled",
@@ -27,9 +27,9 @@ var storageTestCases = []serviceLifecycleTestCase{
 	},
 	{ // General Purpose Storage Account
 		group:     "storage",
-		name:      "general-purpose-account",
-		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
-		planID:    "6ddf6b41-fb60-4b70-af99-8ecc4896b3cf",
+		name:      "general-purpose-v1-account",
+		serviceID: "d10ea062-b627-41e8-a240-543b60030694",
+		planID:    "9364d013-3690-4ce5-b0a2-b43d9b970b02",
 		provisioningParameters: map[string]interface{}{
 			"location":              "southcentralus",
 			"enableNonHttpsTraffic": "disabled",
@@ -42,8 +42,8 @@ var storageTestCases = []serviceLifecycleTestCase{
 	{ // Blob Storage Account
 		group:     "storage",
 		name:      "blob-account",
-		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
-		planID:    "800a17e1-f20a-463d-a290-20516052f647",
+		serviceID: "1a5b4582-29a3-48c5-9cac-511fd8c52756",
+		planID:    "98ae02ec-da21-4b09-b5e0-e2f9583d565c",
 		provisioningParameters: map[string]interface{}{
 			"location":              "eastus",
 			"enableNonHttpsTraffic": "enabled",
@@ -57,9 +57,9 @@ var storageTestCases = []serviceLifecycleTestCase{
 	},
 	{ // Blob Storage Account + Blob Container
 		group:     "storage",
-		name:      "blob-account-with-container",
-		serviceID: "2e2fc314-37b6-4587-8127-8f9ee8b33fea",
-		planID:    "189d3b8f-8307-4b3f-8c74-03d069237f70",
+		name:      "blob-account-all-in-one",
+		serviceID: "d799916e-3faf-4bdf-a48b-bf5012a2d38c",
+		planID:    "6c3b587d-0f88-4112-982a-dbe541f30669",
 		provisioningParameters: map[string]interface{}{
 			"location":              "southcentralus",
 			"enableNonHttpsTraffic": "enabled",
