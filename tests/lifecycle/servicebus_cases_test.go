3b25a45e15b89992cd9fa303cb3bce35dd5ffa02
Tongyao Si <tosi@microsoft.com>
2018-12-05 16:57:22 +0800

    Add lifecycle test for service bus queue and topic

--- tests/lifecycle/servicebus_cases_test.go
+++ tests/lifecycle/servicebus_cases_test.go
@@ -2,16 +2,45 @@
 
 package lifecycle
 
+var serviceBusNamespaceAlias = "test-servicebus-namespace"
 var servicebusTestCases = []serviceLifecycleTestCase{
 	{
 		group:     "servicebus",
-		name:      "servicebus",
+		name:      "servicebus-namespace",
 		serviceID: "6dc44338-2f13-4bc5-9247-5b1b3c5462d3",
-		planID:    "d06817b1-87ea-4320-8942-14b1d060206a",
+		planID:    "6be0d8b5-381f-4d68-bdfd-a131425d3835",
 		provisioningParameters: map[string]interface{}{
 			"location": "southcentralus",
+			"alias":    serviceBusNamespaceAlias,
 			"tags": map[string]interface{}{
 				"latest-operation": "provision",
+			},
+		},
+		childTestCases: []*serviceLifecycleTestCase{
+			{ // queue scenario
+				group:     "servicebus",
+				name:      "servicebus-queue",
+				serviceID: "0e93fbb8-7904-43a5-82db-81c7d3886a24",
+				planID:    "89440eec-a888-49ce-b392-60c653d7a98b",
+				provisioningParameters: map[string]interface{}{
+					"parentAlias":       serviceBusNamespaceAlias,
+					"queueName":         "testqueue",
+					"maxQueueSize":      2048,
+					"messageTimeToLive": "PT24H23M22S",
+					"lockDuration":      "PT2M30S",
+				},
+			},
+			{ // topic scenario
+				group:     "servicebus",
+				name:      "servicebus-topic",
+				serviceID: "dc6d1545-4391-4c7e-ac7e-a8463787fb93",
+				planID:    "dd1e4d44-58be-4f34-84ff-f73ccef405e5",
+				provisioningParameters: map[string]interface{}{
+					"parentAlias":       serviceBusNamespaceAlias,
+					"topicName":         "testtopic",
+					"maxQueueSize":      4096,
+					"messageTimeToLive": "PT276H13M14S",
+				},
 			},
 		},
 	},
