89d88d206ad049a3ee7183058c75d223f37c6a39
root <root@lnxclientVM2.rksut11bsb5uvmivooz3ghwufg.bx.internal.cloudapp.net>
2019-04-12 20:57:46 +0000

    Adding EventHub lifecycle tests;include necessary depencency

--- tests/lifecycle/eventhub_cases_test.go
+++ tests/lifecycle/eventhub_cases_test.go
@@ -2,6 +2,13 @@
 
 package lifecycle
 
+import (
+	"context"
+	"fmt"
+	"github.com/Azure/azure-event-hubs-go"
+	"time"
+)
+
 var eventhubsTestCases = []serviceLifecycleTestCase{
 	{
 		group:     "eventhubs",
@@ -14,5 +21,52 @@ var eventhubsTestCases = []serviceLifecycleTestCase{
 				"latest-operation": "provision",
 			},
 		},
+		testCredentials: testEventhubCreds,
 	},
+}
+
+func testEventhubCreds(credentials map[string]interface{}) error {
+
+	hubName := credentials["eventHubName"].(string)
+	connStrP1 := credentials["connectionString"].(string)
+	connStr := connStrP1 + ";EntityPath=" + hubName
+
+	hub, err := eventhub.NewHubFromConnectionString(connStr)
+
+	if err != nil {
+		return fmt.Errorf("error creating eventhub client object: %s", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 80*time.Second)
+	defer cancel()
+
+	err = hub.Send(ctx, eventhub.NewEventFromString("hello, world!"))
+
+	if err != nil {
+		return fmt.Errorf("error sending message to eventhub: %s", err)
+
+	}
+
+	handler := func(c context.Context, event *eventhub.Event) error {
+		fmt.Println(string(event.Data))
+		return nil
+	}
+
+	runtimeInfo, err := hub.GetRuntimeInformation(ctx)
+
+	if err != nil {
+		return fmt.Errorf("error getting RunTimeInformation from eventhub: %s", err)
+	}
+
+	for _, partitionID := range runtimeInfo.PartitionIDs {
+		_, err := hub.Receive(ctx, partitionID, handler, eventhub.ReceiveWithLatestOffset())
+		if err != nil {
+			return fmt.Errorf("error receiving messages from eventhub: %s", err)
+		}
+
+	}
+
+	hub.Close(context.Background())
+
+	return nil
 }
