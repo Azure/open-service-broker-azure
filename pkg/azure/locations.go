78791e39d0faf0ca91d1e0176137ff7bd92b5e37
Tongyao Si <tosi@microsoft.com>
2019-06-19 17:31:19 +0800

    Add support for mooncake retions

--- pkg/azure/locations.go
+++ pkg/azure/locations.go
@@ -6,7 +6,7 @@ import (
 	"github.com/Azure/open-service-broker-azure/pkg/service"
 )
 
-var locations = []string{
+var azurePublicCloudLocations = []string{
 	"australiaeast",
 	"australiasoutheast",
 	"brazilsouth",
@@ -35,9 +35,33 @@ var locations = []string{
 	"westus2",
 }
 
+var azureChinaCloudLocations = []string{
+	"chinanorth2",
+	"chinanorth",
+	"chinaeast2",
+	"chinaeast",
+}
+
 // IsValidLocation returns a bool indicating whether the provided location is a
 // valid one
 func IsValidLocation(location string) bool {
+	// We can directly ignore returned err here,
+	// because this function is invoked at the start of
+	// OSBA initiating. If there is an error to
+	// invoke this function, OSBA will fail earlier.
+	config, _ := GetConfigFromEnvironment()
+	var locations []string
+	switch config.Environment.Name {
+	case "AzureChinaCloud":
+		locations = azureChinaCloudLocations
+	case "AzurePublicCloud":
+		locations = azurePublicCloudLocations
+	// We shouldn't run into default case, but instead
+	// of raising a panic, we use public cloud locations
+	// and the error can be reported when provisioning the resource.
+	default:
+		locations = azurePublicCloudLocations
+	}
 	for _, l := range locations {
 		if location == l {
 			return true
